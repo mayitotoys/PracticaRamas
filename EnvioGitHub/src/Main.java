0452a18ebf76bbecf8e436a37cf2974f9cfa0a80
unknown <870159626.mty@gmail.com>
2024-10-25 20:46:28 -0600

    programa Si

---
+++ EnvioGitHub/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String box = sc.nextLine();
+        if(box!="Si"){
+            System.out.println("Hola mundo");
+
+
+    }
+        else if(box!="No"){
+            System.out.println(" ");
+        }
+
+
+        }
+
+}
